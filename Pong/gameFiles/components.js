87271358d37651f74078b7c0688144bdb14bb7d4
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2017-02-21 22:22:35 +0100

    Updated Pong to new syntax

--- Pong/gameFiles/components.js
+++ Pong/gameFiles/components.js
@@ -15,10 +15,10 @@ CLOCKWORKRT.components.push([
             {
                 name: "keyboard_down", code: function (event) {
                     if (event.key == 38) {
-                        this.setVar("#y", this.getVar("#y") - 10);
+                        this.var.$y -= 10;
                     }
                     if (event.key == 40) {
-                        this.setVar("#y", this.getVar("#y") + 10);
+                        this.var.$y += 10;
                     }
                     if (event.key == 66) {
                         if (HYPERGAP) {
@@ -30,12 +30,12 @@ CLOCKWORKRT.components.push([
             {
                 name: "gamepadDown", code: function (event) {
                     if (event.name == "DU") {
-                        this.setVar("#y", this.getVar("#y") - 10);
+                        this.var.$y -= 10;
                     }
                     if (event.name == "DD") {
-                        this.setVar("#y", this.getVar("#y") + 10);
+                        this.var.$y += 10;
                     }
-                    if(event.name=="B"){
+                    if (event.name == "B") {
                         if (HYPERGAP) {
                             HYPERGAP.API.loadMenu();
                         }
@@ -45,10 +45,10 @@ CLOCKWORKRT.components.push([
             {
                 name: "updateScore", code: function (event) {
                     if (event.won == "AI") {
-                        this.setVar("#state", "lost");
+                        this.var.$state = "lost";
                         var that = this;
                         setTimeout(function () {
-                            that.setVar("#state", "idle");
+                            that.var.$state = "idle";
                         }, 2000);
                     }
                 }
@@ -61,44 +61,44 @@ CLOCKWORKRT.components.push([
         events: [
             {
                 name: "#setup", code: function (event) {
-                    this.setVar("vy", 0);
+                    this.var.$y = 0;
                 }
             },
             {
                 name: "#loop", code: function (event) {
-                    var ballX = this.engine.find("ball").getVar("#x");
-                    var barX = this.getVar("#x");
-                    var ballY = this.engine.find("ball").getVar("#y");
-                    var barY = this.getVar("#y");
+                    var ballX = this.engine.find("ball").var.$x;
+                    var barX = this.var.$x;
+                    var ballY = this.engine.find("ball").var.$y;
+                    var barY = this.var.$y;
 
                     if (Math.abs(ballX - barX) < 50) {
                         if (Math.abs(ballY - barY) > 4) {
                             if (barY > ballY) {
-                                this.setVar("vy", this.getVar("vy") - 1);
+                                this.var.vy--;
                             } else if (barY < ballY) {
-                                this.setVar("vy", this.getVar("vy") + 1);
+                                this.var.vy++;
                             }
                         }
                     } else {
                         if (barY > 185) {
-                            this.setVar("vy", this.getVar("vy") - 1);
+                            this.var.vy--;
                         } else if (barY < 15) {
-                            this.setVar("vy", this.getVar("vy") + 1);
+                            this.var.vy++;
                         }
                     }
-                    if (Math.abs(this.getVar("vy")) > 2) {
-                        this.setVar("vy", this.getVar("vy") * 2 / 3);
+                    if (Math.abs(this.var.vy) > 2) {
+                        this.var.vy *= 2 / 3;
                     }
-                    this.setVar("#y", this.getVar("#y") + this.getVar("vy"));
+                    // this.var.$y += this.var.vy;
                 }
             },
             {
                 name: "updateScore", code: function (event) {
                     if (event.won == "player") {
-                        this.setVar("#state", "lost");
+                        this.var.$state = "lost";
                         var that = this;
                         setTimeout(function () {
-                            that.setVar("#state", "idle");
+                            that.var.$state = "idle";
                         }, 2000);
                     }
                 }
@@ -111,35 +111,34 @@ CLOCKWORKRT.components.push([
         events: [
             {
                 name: "#setup", code: function (event) {
-                    this.setVar("vx", 1);
-                    this.setVar("vy", 1);
+                    this.var.vx = 1;
+                    this.var.vy = 1;
                 }
             },
             {
                 name: "#loop", code: function (event) {
-                    if (this.getVar("#y") < 0 || this.getVar("#y") > 200) {
-                        this.setVar("vy", -1 * this.getVar("vy"));
-                        // this.setVar("#y", this.getVar("#y") + 2*this.getVar("vy"));
+                    if (this.var.$y < 0 || this.var.$y > 200) {
+                        this.var.vy *= -1;
                     }
-                    this.setVar("#x", this.getVar("#x") + this.getVar("vx"));
-                    this.setVar("#y", this.getVar("#y") + this.getVar("vy"));
+                    this.var.$x += this.var.vx;
+                    this.var.$y += this.var.vy;
 
-                    if (this.getVar("#x") < 5) {
-                        this.setVar("#x", 160);
-                        this.setVar("#y", 50);
-                        this.engine.execute_event("updateScore", { won: "AI" });
+                    if (this.var.$x < 5) {
+                        this.var.$x = 160;
+                        this.var.$y = 50;
+                        this.engine.do.updateScore({ won: "AI" });
                     }
-                    if (this.getVar("#x") > 315) {
-                        this.setVar("#x", 160);
-                        this.setVar("#y", 50);
-                        this.engine.execute_event("updateScore", { won: "player" });
+                    if (this.var.$x > 315) {
+                        this.var.$x = 160;
+                        this.var.$y = 50;
+                        this.engine.do.updateScore({ won: "player" });
                     }
                 }
             },
             {
                 name: "#collide", code: function (event) {
-                    this.setVar("vx", -1 * this.getVar("vx"));
-                    this.setVar("#x", this.getVar("#x") + this.getVar("vx"));
+                    this.var.vx *= -1;
+                    this.var.$x += this.var.vx;
                 }
             }
         ],
@@ -155,17 +154,17 @@ CLOCKWORKRT.components.push([
         events: [
             {
                 name: "#setup", code: function (event) {
-                    this.setVar("$player", 0);
-                    this.setVar("$AI", 0);
+                    this.var.$player = 0;
+                    this.var.$AI = 0;
                 }
             },
             {
                 name: "updateScore", code: function (event) {
                     if (event.won == "player") {
-                        this.setVar("$player", this.getVar("$player") + 1);
+                        this.var.$player++;
                     }
                     if (event.won == "AI") {
-                        this.setVar("$AI", this.getVar("$AI") + 1);
+                        this.var.$AI++;
                     }
                 }
             }
